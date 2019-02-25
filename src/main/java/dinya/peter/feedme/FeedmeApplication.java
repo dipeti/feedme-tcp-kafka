9062df2ce17f966bb57dbbee10efe73b08867761
Peter Dinya <peter.dinya@outlook.com>
2019-02-25 22:37:22 +0000

    git init

---
+++ src/main/java/dinya/peter/feedme/FeedmeApplication.java
@@ -0,0 +1,13 @@
+package dinya.peter.feedme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedmeApplication.class, args);
+    }
+
+}
